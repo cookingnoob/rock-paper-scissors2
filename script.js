c83dfdea08aaf3e92ce5fead52bf55707d87fa7b
alan <alan@iMac-de-alan.local>
2022-11-14 16:24:29 -0600

    Simplify conditions to win or lose

--- script.js
+++ script.js
@@ -21,6 +21,21 @@ function getComputerChoice (){
     round (player, computer)
 }
 
+let playerScoreText = document.querySelector('#playerScore');
+let computerScoreText = document.querySelector('#computerScore');
+let playerScore = 0;
+let computerScore = 0;
+
+playerScoreText.textContent = `${playerScore}`;
+
+
+function scoreCount (playerScore, computerScore){
+    if (playerScore > computerScore){
+        console.log(`your score ${playerScore}, computer score ${computerScore}. You won the match`) 
+    } else if (playerScore < computerScore){
+        console.log(`your score ${playerScore}, computer score ${computerScore}. You lost the match`) 
+    }
+}
 
 
 
@@ -44,31 +59,20 @@ function getComputerChoice (){
 
 function round (player, computer){
     let roundWinnerText = document.querySelector('#roundWinnerText')
-    if (player === 'Rock' && computer === 'Scissors'){
+    if (player === 'Rock' && computer === 'Scissors'|| player === 'Paper' && computer === 'Rock'||player === 'Scissors' && computer === 'Paper'){
         roundWinnerText.textContent = `You win ${player} beats ${computer}`;
         return (++playerScore);
-    } else if (player === 'Paper' && computer === 'Rock'){
-        roundWinnerText.textContent = `You win ${player} beats ${computer}`;
-        return (++playerScore);
-    } else if (player === 'Scissors' && computer === 'Paper'){
-        roundWinnerText.textContent = `You win ${player} beats ${computer}`;
-        return (++playerScore);
-    }else if (computer === 'Rock' && player === 'Scissors'){
+    }else if (computer === 'Rock' && player === 'Scissors'||computer === 'Paper' && player === 'Rock'||computer === 'Scissors' && player === 'Paper'){
         roundWinnerText.textContent = `You lose ${computer} beats ${player}`;
-        return (++computerScore);
-    } else if (computer === 'Paper' && player === 'Rock'){
-        roundWinnerText.textContent = `You lose ${computer} beats ${player}`;
-        return (++computerScore);
-    } else if (computer === 'Scissors' && player === 'Paper'){
-        roundWinnerText.textContent = 'you lose, scissors beats paper';
         return (++computerScore);
     }  else if (computer === player) {
         roundWinnerText.textContent = 'its a tie';
     } else {
         roundWinnerText.textContent = `wrong input`
     }
-    
     }
+    
+    
 
 
 
@@ -76,30 +80,6 @@ function round (player, computer){
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-let playerScore = 0;
-let computerScore = 0;
 
 // function game (){ 
 // for (i = 0; i < 5; ++i){ 
@@ -109,22 +89,6 @@ let computerScore = 0;
 //     // console.log(computer, player, playerScore, computerScore);
 // }
 // };
-
-
-function declareWinner (playerScore, computerScore){
-    if (playerScore > computerScore){
-        console.log(`your score ${playerScore}, computer score ${computerScore}. You won the match`) 
-    } else if (playerScore < computerScore){
-        console.log(`your score ${playerScore}, computer score ${computerScore}. You lost the match`) 
-    }
-}
-declareWinner(playerScore, computerScore);
-
-
-
-
-
-
 
 
 
